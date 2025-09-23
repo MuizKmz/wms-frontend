667867d16d0fc635f49fdbeb91dfd78ab1857d22
Irfanafifromzi <irfanafifiromzi@gmail.com>
2025-09-23 12:16:12 +0800

    Machine List UI

--- src/views/Machine,tool&mould/MachineList.vue
+++ src/views/Machine,tool&mould/MachineList.vue
@@ -2,19 +2,19 @@
   <AdminLayout>
     <PageBreadcrumb :pageTitle="currentPageTitle" />
     <PageTitle title="Machine Management"/>
-    <WorkOrderFilters @filter-change="handleFilterChange" />
+    <MachineListFilters @filter-change="handleFilterChange" />
     
     <div class="space-y-5 sm:space-y-6">
       <ComponentCard title="All Machine List" desc="Overview of all Machine List">
         <!-- Button in header slot -->
         <template #headerAction>
-          <button class="px-4 py-2  btn btn-accent text-white text-sm font-medium rounded-lg transition-colors duration-200">
+          <button class="px-4 py-2 btn btn-accent text-white text-sm font-medium rounded-lg transition-colors duration-200">
             Add New Machine
           </button>
         </template>
         
         <!-- Table in main slot -->
-        <WorkOrderTable :filters="activeFilters" />
+        <MachineListTable :filters="activeFilters" />
       </ComponentCard>
     </div>
   </AdminLayout>
@@ -26,8 +26,8 @@ import PageBreadcrumb from "@/components/common/PageBreadcrumb.vue";
 import AdminLayout from "@/components/layout/AdminLayout.vue";
 import ComponentCard from "@/components/common/ComponentCard.vue";
 import PageTitle from "@/components/common/PageTitle.vue"; 
-import WorkOrderFilters from "@/components/common/WorkOrderFilters.vue"; // Add this import
-import WorkOrderTable from "@/components/tables/basic-tables/WorkOrderTable.vue";
+import MachineListTable from "@/components/tables/basic-tables/MachineListTable.vue";
+import MachineListFilters from "@/components/common/MachineListFilters.vue";
 
 const currentPageTitle = ref("Machine Management");
 const activeFilters = ref({});
